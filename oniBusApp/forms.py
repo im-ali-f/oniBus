b79fdff4b86d9e55a103a9d3290877fa2adf2dc9
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-08-04 13:14:59 +0330

    lots of html changes + base template created

--- oniBusApp/forms.py
+++ oniBusApp/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class searchForm(forms.Form):
     originDestination=forms.CharField(label="مبدا" ,required=True ,error_messages={"required":"لطفا مبدا را وارد کنید"}, widget=forms.TextInput(attrs={"class":"searchInput","id":"originDest"}))
-    finalDestination=forms.CharField(label="مقصد",required=True,error_messages={"required":"لطفا مقصد را وارد کنید"},widget=forms.TextInput(attrs={"class":"searchInput","id":"originDest"}))
+    finalDestination=forms.CharField(label="مقصد",required=True,error_messages={"required":"لطفا مقصد را وارد کنید"},widget=forms.TextInput(attrs={"class":"searchInput","id":"finalDest"}))
