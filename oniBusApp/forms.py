e68f14b92b7e6e075380cf03e25573894f5791c2
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-08-06 00:04:02 +0330

    jalali date added to project

--- oniBusApp/forms.py
+++ oniBusApp/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-
+from jalali_date.fields import JalaliDateField, SplitJalaliDateTimeField
+from jalali_date.widgets import AdminJalaliDateWidget, AdminSplitJalaliDateTime
 
 class searchForm(forms.Form):
     originDestination=forms.CharField(label="مبدا" ,required=True ,error_messages={"required":"لطفا مبدا را وارد کنید"}, widget=forms.TextInput(attrs={"class":"searchInput","id":"originDest"}))
     finalDestination=forms.CharField(label="مقصد",required=True,error_messages={"required":"لطفا مقصد را وارد کنید"},widget=forms.TextInput(attrs={"class":"searchInput","id":"finalDest"}))
+    dateDeparture=JalaliDateField(label='تاریخ',required=True,error_messages={"required":"لطفا تاریخ را وارد کنید"},widget=AdminJalaliDateWidget(attrs={"id":"dateDeparture"}))
