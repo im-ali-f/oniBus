8ec35504e2418ac7d018c90adc471d3b62429b8b
Ali Farhad <115785932+im-ali-f@users.noreply.github.com>
2023-08-08 13:35:58 +0330

    filter tickets with dateDeparture value

--- oniBusApp/views.py
+++ oniBusApp/views.py
@@ -13,17 +13,23 @@ def viewIndex(request):
 def viewSearch(request):
 
     data=[
-        {"ticketId":1,"origin":"qazvin","destination":"tehran","busType":"volvo","busOrCompanyImg":"","departure":"20:11","price":"210000","isActive":"active"},
-        {"ticketId": 2, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"", "departure": "20:11","price": "210000", "isActive": "deactive"},
-        {"ticketId": 3, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"", "departure": "20:11","price": "210000", "isActive": "active"},
-        {"ticketId": 4, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"", "departure": "20:11","price": "210000", "isActive": "active"},
-        {"ticketId": 5, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"", "departure": "20:11","price": "210000", "isActive": "deactive"},
+        {"ticketId":1,"origin":"qazvin","destination":"tehran","busType":"volvo","busOrCompanyImg":"","dateDeparture":"1402-05-09","departure":"20:11","price":"210000","isActive":"active"},
+        {"ticketId": 2, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"","dateDeparture":"1402-05-09", "departure": "20:11","price": "210000", "isActive": "deactive"},
+        {"ticketId": 3, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"","dateDeparture":"1402-05-09", "departure": "20:11","price": "210000", "isActive": "active"},
+        {"ticketId": 4, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"","dateDeparture":"1402-05-08", "departure": "20:11","price": "210000", "isActive": "active"},
+        {"ticketId": 5, "origin": "qazvin", "destination": "tehran", "busType": "volvo","busOrCompanyImg":"","dateDeparture":"1402-05-08", "departure": "20:11","price": "210000", "isActive": "deactive"},
     ]
     reqData = request.POST
+    newData=[]
+    for aData in data:
+        print(aData["dateDeparture"])
+        if aData["dateDeparture"] == reqData["dateDeparture"]:
+            newData.append(aData)
     context={
         "origin":reqData["originDestination"],
         "destination":reqData["finalDestination"],
-        "data":data
+        "dateDeparture":reqData["dateDeparture"],
+        "data":newData
     }
     print(context)
     return render(request,"oniBusApp/searchPage.html",context)
